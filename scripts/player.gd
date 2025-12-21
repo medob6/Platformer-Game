6afa32422061bbc6e9d557f7c2445409ddcd9cb8
Mohamed Boussetta <mbousset@c4r1p9.1337.ma>
2025-12-21 04:33:47 +0100

    base logic nearly done

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+const PUSH_FORCE := 25.0
+const MIN_PUSH_FORCE := 20.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,8 +15,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Horizontal movement
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +23,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# PUSH RIGID BODIES (this MUST be inside the function)
+	for i in range(get_slide_collision_count()):
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			var push_force = (PUSH_FORCE * velocity.length() / SPEED) + MIN_PUSH_FORCE
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
